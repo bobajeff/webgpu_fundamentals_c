f70d7469ed98b9b7ecdcaa81af89e03c5c0db58f
Jeffrey Brusseau <bobajeff82@gmail.com>
2023-08-31 08:45:24 -0500

    WebGPU Matrix Math part 6

---
+++ 3d_math/04_matrix_math/webgpu-matrix-math-transform-trs.c
@@ -0,0 +1,469 @@
+#include "create_surface.h"
+#include "framework.h"
+#include "webgpu.h"
+#include <GLFW/glfw3.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <time.h>
+
+
+// create a struct to hold the values for the uniforms
+typedef struct Uniforms {
+  float color[4];
+  float matrix[12];
+} Uniforms;
+
+void multiply_3x3_mat(float * a, float * b, float * dest) {
+  float a00 = a[0 * 4 + 0];
+  float a01 = a[0 * 4 + 1];
+  float a02 = a[0 * 4 + 2];
+  float a10 = a[1 * 4 + 0];
+  float a11 = a[1 * 4 + 1];
+  float a12 = a[1 * 4 + 2];
+  float a20 = a[2 * 4 + 0];
+  float a21 = a[2 * 4 + 1];
+  float a22 = a[2 * 4 + 2];
+  float b00 = b[0 * 4 + 0];
+  float b01 = b[0 * 4 + 1];
+  float b02 = b[0 * 4 + 2];
+  float b10 = b[1 * 4 + 0];
+  float b11 = b[1 * 4 + 1];
+  float b12 = b[1 * 4 + 2];
+  float b20 = b[2 * 4 + 0];
+  float b21 = b[2 * 4 + 1];
+  float b22 = b[2 * 4 + 2];
+
+  dest[0] = b00 * a00 + b01 * a10 + b02 * a20;
+  dest[1] = b00 * a01 + b01 * a11 + b02 * a21;
+  dest[2] = b00 * a02 + b01 * a12 + b02 * a22;
+  dest[4] = b10 * a00 + b11 * a10 + b12 * a20;
+  dest[5] = b10 * a01 + b11 * a11 + b12 * a21;
+  dest[6] = b10 * a02 + b11 * a12 + b12 * a22;
+  dest[8] = b20 * a00 + b21 * a10 + b22 * a20;
+  dest[9] = b20 * a01 + b21 * a11 + b22 * a21;
+  dest[10] = b20 * a02 + b21 * a12 + b22 * a22;
+}
+
+void projection_3x3_mat(int width, int height, float * dest) {
+    // Note: This matrix flips the Y axis so that 0 is at the top.
+    dest[0] = 2.0 / width, dest[1] = 0.0,           dest[2] = 0.0, 
+    dest[4] = 0.0,         dest[5] = -2.0 / height, dest[6] = 0.0, 
+    dest[8] = -1.0,        dest[9] = 1.0,           dest[10] = 1.0;
+}
+
+void identity_3x3_mat(float * dest) {
+    dest[0] = 1.0, dest[1] = 0.0, dest[2] = 0.0, 
+    dest[4] = 0.0, dest[5] = 1.0, dest[6] = 0.0, 
+    dest[8] = 0.0, dest[9] = 0.0, dest[10] = 1.0;
+}
+
+void translation_3x3_mat(float tx, float ty, float * dest) {
+    dest[0] = 1.0, dest[1] = 0.0, dest[2] = 0.0, 
+    dest[4] = 0.0, dest[5] = 1.0, dest[6] = 0.0, 
+    dest[8] = tx,  dest[9] = ty,  dest[10] = 1.0;
+}
+
+void rotation_3x3_mat(float angleInRadians, float * dest) {
+    float c = cosf(angleInRadians);
+    float s = sinf(angleInRadians);
+    dest[0] = c,   dest[1] = s,   dest[2] = 0.0, 
+    dest[4] = -s,  dest[5] = c,   dest[6] = 0.0, 
+    dest[8] = 0.0, dest[9] = 0.0, dest[10] = 1.0;
+}
+
+void scaling_3x3_mat(float sx, float sy, float * dest) {
+    dest[0] = sx,  dest[1] = 0.0, dest[2] = 0.0, 
+    dest[4] = 0.0, dest[5] = sy,  dest[6] = 0.0, 
+    dest[8] = 0.0, dest[9] = 0.0, dest[10] = 1.0;
+}
+
+void translate_3x3_mat(float * m, float tx, float ty, float * dst) {
+    float temp_matrix[12];
+    translation_3x3_mat(tx, ty, temp_matrix);
+    multiply_3x3_mat(m, temp_matrix, dst);
+}
+
+void rotate_3x3_mat(float * m, float angleInRadians, float * dst) {
+    float temp_matrix[12];
+    rotation_3x3_mat(angleInRadians, temp_matrix);
+    multiply_3x3_mat(m, temp_matrix, dst);
+}
+
+void scale_3x3_mat(float * m, float sx, float sy, float * dst) {
+    float temp_matrix[12];
+    scaling_3x3_mat(sx, sy, temp_matrix);
+    multiply_3x3_mat(m, temp_matrix, dst);
+}
+
+
+
+int main(int argc, char *argv[]) {
+  srand(time(NULL)); // seed random number generator
+
+  initializeLog();
+
+  WGPUInstance instance =
+      wgpuCreateInstance(&(WGPUInstanceDescriptor){.nextInChain = NULL});
+
+  WGPUAdapter adapter;
+  wgpuInstanceRequestAdapter(instance, NULL, request_adapter_callback,
+                             (void *)&adapter);
+
+  WGPUDevice device;
+  wgpuAdapterRequestDevice(adapter, NULL, request_device_callback,
+                           (void *)&device);
+
+  WGPUQueue queue = wgpuDeviceGetQueue(device);
+
+  wgpuDeviceSetUncapturedErrorCallback(device, handle_uncaptured_error, NULL);
+
+  // Create GLFW Window and use as WebGPU surface
+  if (!glfwInit()) {
+    printf("Cannot initialize glfw");
+    return 1;
+  }
+
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow *window = glfwCreateWindow(640, 480, "wgpu with glfw", NULL, NULL);
+
+  if (!window) {
+    printf("Cannot create window");
+    return 1;
+  }
+  WGPUSurface surface = create_surface(instance, window);
+  WGPUTextureFormat presentationFormat =
+      wgpuSurfaceGetPreferredFormat(surface, adapter);
+
+  WGPUShaderModuleDescriptor shaderSource = load_wgsl(
+      RESOURCE_DIR "webgpu-matrix-math-transform-just-matrix-3x3.wgsl");
+  WGPUShaderModule module = wgpuDeviceCreateShaderModule(device, &shaderSource);
+
+  WGPURenderPipeline pipeline = wgpuDeviceCreateRenderPipeline(
+      device,
+      &(WGPURenderPipelineDescriptor){
+          .label = "just 2d position",
+          .vertex =
+              (WGPUVertexState){
+                  .module = module,
+                  .entryPoint = "vs",
+                  .bufferCount = 1,
+                  .buffers =
+                      (WGPUVertexBufferLayout[]){
+                          {.arrayStride = 2 * 4, // (2) floats, 4 bytes each
+                           .attributes =
+                               (WGPUVertexAttribute[]){
+                                   // position
+                                   {.shaderLocation = 0,
+                                    .offset = 0,
+                                    .format = WGPUVertexFormat_Float32x2},
+                               },
+                           .attributeCount = 1,
+                           .stepMode = WGPUVertexStepMode_Vertex},
+                         },},
+          .primitive =
+              (WGPUPrimitiveState){
+                  .topology = WGPUPrimitiveTopology_TriangleList,
+                  .stripIndexFormat = WGPUIndexFormat_Undefined,
+                  .frontFace = WGPUFrontFace_CCW,
+                  .cullMode = WGPUCullMode_None},
+          .multisample =
+              (WGPUMultisampleState){
+                  .count = 1,
+                  .mask = ~0,
+                  .alphaToCoverageEnabled = false,
+              },
+          .fragment =
+              &(WGPUFragmentState){
+                  .module = module,
+                  .entryPoint = "fs",
+                  .targetCount = 1,
+                  .targets =
+                      &(WGPUColorTargetState){
+                          .format = presentationFormat,
+                          .blend =
+                              &(WGPUBlendState){
+                                  .color =
+                                      (WGPUBlendComponent){
+                                          .srcFactor = WGPUBlendFactor_One,
+                                          .dstFactor = WGPUBlendFactor_Zero,
+                                          .operation = WGPUBlendOperation_Add,
+                                      },
+                                  .alpha =
+                                      (WGPUBlendComponent){
+                                          .srcFactor = WGPUBlendFactor_One,
+                                          .dstFactor = WGPUBlendFactor_Zero,
+                                          .operation = WGPUBlendOperation_Add,
+                                      }},
+                          .writeMask = WGPUColorWriteMask_All,
+                      },
+              },
+          .depthStencil = NULL,
+      });
+
+
+  // color, matrix
+  const uint64_t uniformBufferSize = (4 + 12) * 4;
+  const WGPUBuffer uniformBuffer = wgpuDeviceCreateBuffer(
+      device, &(WGPUBufferDescriptor){.label = "uniforms",
+                                      .size = uniformBufferSize,
+                                      .usage = WGPUBufferUsage_Uniform |
+                                               WGPUBufferUsage_CopyDst,
+                                      .mappedAtCreation = false});
+
+  Uniforms uniformValues = {.color = {(float)rand()/(float)(RAND_MAX/1.0), (float)rand()/(float)(RAND_MAX/1.0), (float)rand()/(float)(RAND_MAX/1.0), 1}};
+
+
+  // createFVertices()
+  float vertexData[] = {
+    // left column
+    0, 0,
+    30, 0,
+    0, 150,
+    30, 150,
+
+    // top rung
+    30, 0,
+    100, 0,
+    30, 30,
+    100, 30,
+
+    // middle rung
+    30, 60,
+    70, 60,
+    30, 90,
+    70, 90,
+  };
+  int vertexDataSize = sizeof(vertexData);
+  u_int32_t indexData[] = {
+    0,  1,  2,    2,  1,  3,  // left column
+    4,  5,  6,    6,  5,  7,  // top run
+    8,  9, 10,   10,  9, 11,  // middle run
+  };
+  int indexDataSize = sizeof(indexData);
+  int numVertices = sizeof(indexData) / sizeof(u_int32_t);
+
+  const WGPUBuffer vertexBuffer = wgpuDeviceCreateBuffer(
+      device, &(WGPUBufferDescriptor){.label = "vertex buffer vertices",
+                                      .size = vertexDataSize,
+                                      .usage = WGPUBufferUsage_Vertex |
+                                               WGPUBufferUsage_CopyDst,
+                                      .mappedAtCreation = false});
+  wgpuQueueWriteBuffer(queue, vertexBuffer, 0, vertexData,
+
+                       vertexDataSize);
+  const WGPUBuffer indexBuffer = wgpuDeviceCreateBuffer(
+      device, &(WGPUBufferDescriptor){.label = "index buffer",
+                                      .size = indexDataSize,
+                                      .usage = WGPUBufferUsage_Index |
+                                               WGPUBufferUsage_CopyDst,
+                                      .mappedAtCreation = false});
+  wgpuQueueWriteBuffer(queue, indexBuffer, 0, indexData,
+                       indexDataSize);
+
+  WGPUBindGroup bindGroup = wgpuDeviceCreateBindGroup(
+      device,
+      &(WGPUBindGroupDescriptor){
+          .label = "bind group for object",
+          .layout = wgpuRenderPipelineGetBindGroupLayout(pipeline, 0),
+          .entries = (WGPUBindGroupEntry[]){{.binding = 0,
+                                              .buffer = uniformBuffer,
+                                              .size = uniformBufferSize,
+                                              .offset = 0},},
+          .entryCount = 1});
+
+  WGPUSwapChainDescriptor config = (WGPUSwapChainDescriptor){
+      .nextInChain =
+          (const WGPUChainedStruct *)&(WGPUSwapChainDescriptorExtras){
+              .chain =
+                  (WGPUChainedStruct){
+                      .next = NULL,
+                      .sType = (WGPUSType)WGPUSType_SwapChainDescriptorExtras,
+                  },
+              .alphaMode = WGPUCompositeAlphaMode_Auto,
+              .viewFormatCount = 0,
+              .viewFormats = NULL,
+          },
+      .usage = WGPUTextureUsage_RenderAttachment,
+      .format = presentationFormat,
+      .width = 0,
+      .height = 0,
+      .presentMode = WGPUPresentMode_Fifo,
+  };
+
+  glfwGetWindowSize(window, (int *)&config.width, (int *)&config.height);
+
+  WGPUSwapChain swapChain = wgpuDeviceCreateSwapChain(device, surface, &config);
+
+  WGPUSupportedLimits limits = {};
+  bool gotlimits = wgpuDeviceGetLimits(device, &limits);
+  
+  #define NUM_OF_PRESETS 10
+  float translation[NUM_OF_PRESETS][2] = {
+    {150.0,100.0},
+    {200.0,300.0},
+    {500.0,40.0},
+    {700.0,1000.0},
+    {300.0,200.0},
+    {40.0,500.0},
+    {1000.0,700.0},
+    {80.0,20.0},
+    {10.0,250.0},
+    {1000.0,1000.0},
+  };
+
+  float rotation_degree = 0;
+  int preset = 0;
+  int count = 0;
+
+  float scale[2] = {1.0,1.0};
+  int scale_forward = 1;
+  int scale_axis = 0;
+  
+  while (!glfwWindowShouldClose(window)) {
+    // change translation preset after 250 loops (because making a GUI is not as easy outside of the web)
+    if (count < 250) 
+    {
+        count++;
+    }
+    else {
+        scale_axis = scale_axis == 0 ? 1 : 0; //switch scale axis every 250 loop
+        count = 0;
+        // if (preset < NUM_OF_PRESETS + 1){
+        //     preset++;
+        // }
+        // else {
+        //     preset = 0;
+        // }
+        // printf("translation: %f,%f\n", translation[preset][0],translation[preset][1]);
+    }
+    // if (rotation_degree < 90){
+    //     rotation_degree += 0.1;
+    // } else {
+    //     rotation_degree = 0;
+    // }
+    // printf("rotation: %f°\n", rotation_degree);
+
+    if (scale_forward){
+        if (scale[scale_axis] < 5){
+            scale[scale_axis] += 0.1;
+        } else {
+            scale_forward = 0;
+        }
+    } else {
+        if (scale[scale_axis] > -5){
+            scale[scale_axis] -= 0.1;
+        } else {
+            scale_forward = 1;
+        }
+    }
+    printf("scale: %f,%f\n", scale[0],scale[1]);
+    
+
+
+    WGPUTextureView view = NULL;
+
+    for (int attempt = 0; attempt < 2; attempt++) {
+      uint32_t prevWidth = config.width;
+      uint32_t prevHeight = config.height;
+      glfwGetWindowSize(window, (int *)&config.width, (int *)&config.height);
+
+      if (prevWidth != config.width || prevHeight != config.height) {
+        swapChain = wgpuDeviceCreateSwapChain(device, surface, &config);
+      }
+      // Get the current texture from the swapChain to use for rendering to by
+      // the render pass
+      view = wgpuSwapChainGetCurrentTextureView(swapChain);
+      if (attempt == 0 && !view) {
+        printf("wgpuSwapChainGetCurrentTextureView() failed; trying to create "
+               "a new swap chain...\n");
+        config.width = 0;
+        config.height = 0;
+        continue;
+      }
+
+      break;
+    }
+
+    if (!view) {
+      printf("Cannot acquire next swap chain texture\n");
+      return 1;
+    }
+
+    // make a command encoder to start encoding commands
+    WGPUCommandEncoder encoder = wgpuDeviceCreateCommandEncoder(
+        device, &(WGPUCommandEncoderDescriptor){.label = "our encoder"});
+
+    WGPURenderPassDescriptor renderPassDescriptor = {
+        .colorAttachments =
+            &(WGPURenderPassColorAttachment){
+                .view = view, // texture from SwapChain
+                .resolveTarget = NULL,
+                .loadOp = WGPULoadOp_Clear,
+                .storeOp = WGPUStoreOp_Store,
+                .clearValue =
+                    (WGPUColor){
+                        .r = 0.3,
+                        .g = 0.3,
+                        .b = 0.3,
+                        .a = 1.0,
+                    },
+            },
+        .colorAttachmentCount = 1,
+        .depthStencilAttachment = NULL,
+    };
+
+    // make a render pass encoder to encode render specific commands
+    WGPURenderPassEncoder pass =
+        wgpuCommandEncoderBeginRenderPass(encoder, &renderPassDescriptor);
+    wgpuRenderPassEncoderSetPipeline(pass, pipeline);
+    wgpuRenderPassEncoderSetVertexBuffer(pass, 0, vertexBuffer, 0,
+                                         vertexDataSize);
+    wgpuRenderPassEncoderSetIndexBuffer(pass, indexBuffer, WGPUIndexFormat_Uint32, 0, indexDataSize);
+    int window_width, window_height;
+
+    glfwGetWindowSize(window, (int *)&window_width, (int *)&window_height);
+
+    window_width = (window_width < limits.limits.maxTextureDimension2D)
+                       ? window_width
+                       : limits.limits.maxTextureDimension2D;
+    window_height = (window_height < limits.limits.maxTextureDimension2D)
+                        ? window_height
+                        : limits.limits.maxTextureDimension2D;
+
+    window_width = (window_width > 1) ? window_width : 1;
+    window_height = (window_height > 1) ? window_height : 1;
+
+    float tmp_matrix[12];
+    projection_3x3_mat(window_width, window_height, tmp_matrix);
+    float tmp_matrix_2[12];
+    translate_3x3_mat(tmp_matrix, translation[preset][0], translation[preset][1], tmp_matrix_2);
+    float tmp_matrix_3[12];
+    rotate_3x3_mat(tmp_matrix, rotation_degree, tmp_matrix_3);
+    scale_3x3_mat(tmp_matrix_3, scale[0], scale[1], uniformValues.matrix);
+
+    // upload the uniform values to the uniform buffer
+    wgpuQueueWriteBuffer(queue, uniformBuffer, 0, &uniformValues,
+                         sizeof(uniformValues));
+
+    wgpuRenderPassEncoderSetBindGroup(pass, 0, bindGroup, 0, NULL);
+    wgpuRenderPassEncoderDrawIndexed(pass, numVertices, 1, 0, 0, 0);
+    wgpuRenderPassEncoderEnd(pass);
+
+    WGPUQueue queue = wgpuDeviceGetQueue(device);
+    WGPUCommandBuffer commandBuffer = wgpuCommandEncoderFinish(
+        encoder, &(WGPUCommandBufferDescriptor){.label = NULL});
+    wgpuQueueSubmit(queue, 1, &commandBuffer);
+    wgpuSwapChainPresent(swapChain);
+
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
